48f149534086af14499987940eacaf318407014b
Peppe_dev <giuseppebarra104@gmail.com>
2026-06-15 19:40:45 +0000

    Refactor area calculation for various shapes

--- aree.cpp
+++ aree.cpp
@@ -1,85 +1,94 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+
 using namespace std;
+
 int main() 
 {
-    cout << "inserisci il nome della figura" << endl;
-    string nome_figura;
-    cin >> nome_figura;
-    if (nome_figura == "triangolo")
+    cout << "Enter the name of the shape:" << endl;
+    string shape_name;
+    cin >> shape_name;
+
+    // --- TRIANGLE ---
+    if (shape_name == "triangle")
     {
-        float lato;
-        float altezza;
-        cout << "inserisci il lato" << endl;
-        cin >> lato;
-        cout << "inserisci l'altezza" << endl;
-        cin >> altezza;
-        cout << (lato * altezza / 2) << endl;
+        float side;
+        float height;
+        cout << "Enter the side:" << endl;
+        cin >> side;
+        cout << "Enter the height:" << endl;
+        cin >> height;
+        cout << "The area is: " << (side * height / 2) << endl;
         return 0;
-        
     }
- else if  (nome_figura == "rettangolo")
-{
-    float lato2;
-    float altezza2;
-    cout << "inserisci il lato " << endl;
-    cin >> lato2;
-    cout << "inserisci l'altezza" << endl;
-    cin >> altezza2;
-    cout << (lato2 * altezza2) << endl;
+    // --- RECTANGLE ---
+    else if (shape_name == "rectangle")
+    {
+        float side2;
+        float height2;
+        cout << "Enter the side:" << endl;
+        cin >> side2;
+        cout << "Enter the height:" << endl;
+        cin >> height2;
+        cout << "The area is: " << (side2 * height2) << endl;
+        return 0;
+    }
+    // --- SQUARE ---
+    else if (shape_name == "square")
+    {
+        float side3;
+        cout << "Enter the side:" << endl;
+        cin >> side3;
+        cout << "The area is: " << pow(side3, 2) << endl;
+        return 0;
+    }
+    // --- PARALLELOGRAM ---
+    else if (shape_name == "parallelogram")
+    {
+        float side4;
+        float height4;
+        cout << "Enter the side:" << endl;
+        cin >> side4;
+        cout << "Enter the height:" << endl;
+        cin >> height4;
+        cout << "The area is: " << (side4 * height4) << endl;
+        return 0;
+    } 
+    // --- TRAPEZOID ---
+    else if (shape_name == "trapezoid")
+    {
+        float major_base;
+        float minor_base;
+        float height6;
+        cout << "Enter the major base:" << endl;
+        cin >> major_base;
+        cout << "Enter the minor base:" << endl;
+        cin >> minor_base;
+        cout << "Enter the height:" << endl;
+        cin >> height6;
+        float sum = major_base + minor_base;
+        cout << "The area is: " << (sum * height6 / 2) << endl;
+        return 0;
+    }
+    // --- RHOMBUS ---
+    else if (shape_name == "rhombus")
+    {
+        float side5;
+        float height7;
+        cout << "Enter the side:" << endl;
+        cin >> side5;
+        cout << "Enter the height:" << endl;
+        cin >> height7;
+        cout << "The area is: " << side5 * height7 << endl;
+        return 0;
+    }
+    // --- INVALID INPUT ---
+    else
+    {
+        cout << "Invalid shape or not yet supported. Here is the list of available shapes:" << endl;
+        cout << "triangle, rectangle, square, parallelogram, rhombus, trapezoid" << endl;
+        return 0;
+    }
     return 0;
-}
-else if (nome_figura == "quadrato")
-{
- float lato3;
- cout << "inserisci il lato" << endl;
- cin >> lato3;
- cout << pow(lato3 , 2) << endl;
- return 0;
-}
-else if (nome_figura == "parallelogrammo")
-{
-    float lato4;
-    float altezza4;
-    cout << "inserisci il lato" << endl;
-    cin >> lato4;
-    cout << "inserisci l' altezza" << endl;
-    cin >> altezza4;
-    cout << (lato4 * altezza4);
-    return 0;
-} 
-else if (nome_figura == "trapezio")
-{
-    float basemaggiore;
-    float baseminore;
-    float altezza6;
-    cout <<"inserisci la base maggiore" << endl;
-    cin >> basemaggiore;
-    cout << "inserisci la base minore" << endl;
-    cin >> baseminore;
-    cout <<"inserisci l'altezza" << endl;
-    cin >> altezza6;
-    float somma = basemaggiore + baseminore;
-    cout << ( somma * altezza6 / 2) << endl;
-    return 0;
-}
-else if (nome_figura == "rombo")
-{
-    float lato5;
-    float altezza7;
-    cout << "inserisci il lato" << endl;
-    cin >> lato5;
-    cout << "inserisci l'altezza" << endl;
-    cin >> altezza7;
-    cout << lato5 * altezza7 << endl;
-    return 0;
-}
-else
-{
-    std::cout << "figura non valida o non ancora inserita, questa è la lista di figure inserite :" << std::endl;
-    std::cout << "triangolo, rettangolo, quadrato, parallelogrammo, rombo, trapezio" << std::endl;
-    return 0;
-}
-return 0;
 }
